b0217d4d5712b088447d858c6e6fd390d3195617
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-03-15 15:44:14 +0400

    feat/login and get user

--- nuxt/pages/login.vue
+++ nuxt/pages/login.vue
@@ -1,6 +1,54 @@
 <script setup lang="ts">
+const form = ref({
+  email: "test@example.com",
+  password: "password",
+});
+
+const handleSubmit = async () => {
+  await useFetch("http://localhost:8000/sanctum/csrf-cookie", {
+    credentials: "include",
+  });
+
+  const token = useCookie("XSRF-TOKEN");
+
+  await useFetch("http://localhost:8000/login", {
+    credentials: "include",
+    method: "POST",
+    body: form.value,
+    watch: false,
+    headers: {
+      "X-XSRF-TOKEN": token.value as string,
+    },
+  });
+
+  const { data } = await useFetch("http://localhost:8000/api/user", {
+    credentials: "include",
+    watch: false,
+    headers: {
+      "X-XSRF-TOKEN": token.value as string,
+    },
+  });
+
+  console.log(data);
+};
 </script>
 
 <template>
-  <h1>Login Page</h1>
+  <form @submit.prevent="handleSubmit">
+    <label>
+      Email:
+      <input type="text" name="email" v-model="form.email" />
+    </label>
+    <label>
+      Password:
+      <input type="password" name="password" v-model="form.password" />
+    </label>
+    <button type="submit">Login</button>
+  </form>
 </template>
+
+<style scoped>
+input {
+  background-color: gray;
+}
+</style>
